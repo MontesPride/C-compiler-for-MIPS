056fb54fdb34e8950d0df14060d30354cd60e6cd
Szymon Filipiak <szymonfilipiak98@gmail.com>
2019-10-23 15:54:08 +0200

    Name analysis, initial commit.

--- src/sem/NameAnalysisVisitor.java
+++ src/sem/NameAnalysisVisitor.java
@@ -4,165 +4,256 @@ import ast.*;
 
 public class NameAnalysisVisitor extends BaseSemanticVisitor<Void> {
 
-	@Override
-	public Void visitBaseType(BaseType bt) {
-		// To be completed...
-		return null;
-	}
+    public Scope scope;
 
-	@Override
-	public Void visitStructTypeDecl(StructTypeDecl sts) {
-		// To be completed...
-		return null;
-	}
+    public NameAnalysisVisitor() {
+        this.scope = new Scope();
+    }
 
-	@Override
-	public Void visitBlock(Block b) {
-		// To be completed...
-		return null;
-	}
+    public NameAnalysisVisitor(Scope scope) {
+        this.scope = scope;
+    }
 
-	@Override
-	public Void visitFunDecl(FunDecl p) {
-		// To be completed...
-		return null;
-	}
+    @Override
+    public Void visitBaseType(BaseType bt) {
+        // To be completed...
+        return null;
+    }
 
+    @Override
+    public Void visitStructTypeDecl(StructTypeDecl sts) {
+        Symbol s = scope.lookupCurrent(sts.structType.name);
+        if (s != null)
+        	error("StructType" + sts.structType.name + "already declared");
+        else
+        	scope.put(new StructTypeSymbol(sts));
+        return null;
+    }
 
-	@Override
-	public Void visitProgram(Program p) {
-		// To be completed...
-		return null;
-	}
+    @Override
+    public Void visitBlock(Block b) {
+        Scope oldScope = scope;
+        scope = new Scope(oldScope);
+        for (VarDecl vd : b.variables)
+            visitVarDecl(vd);
+        for (Stmt stmt : b.statements)
+            visitStatement(stmt);
+        scope = oldScope;
+        return null;
+    }
 
-	@Override
-	public Void visitVarDecl(VarDecl vd) {
-		// To be completed...
-		return null;
-	}
+    public void visitStatement(Stmt stmt) {
+        switch (stmt.getClass().getSimpleName()) {
+            case "Block": {
+                visitBlock((Block) stmt);
+                break;
+            }
+            case "While": {
+                visitWhile((While) stmt);
+                break;
+            }
+            case "If": {
+                visitIf((If) stmt);
+                break;
+            }
+            case "Assign": {
+                visitAssign((Assign) stmt);
+                break;
+            }
+            case "Return": {
+                visitReturn((Return) stmt);
+                break;
+            }
+            case "ExprStmt": {
+                visitExprStmt((ExprStmt) stmt);
+                break;
+            }
+        }
+    }
 
-	@Override
-	public Void visitVarExpr(VarExpr v) {
-		// To be completed...
-		return null;
-	}
+    @Override
+    public Void visitFunDecl(FunDecl fd) {
+        Symbol s = scope.lookupCurrent(fd.name);
+        if (s != null)
+            error("Function " + fd.name + " already declared");
+        else
+            scope.put(new FuncSymbol(fd));
 
-	@Override
-	public Void visitFunCallExpr(FunCallExpr fc) {
-		// To be completed...
-		return null;
-	}
+        for (VarDecl vd : fd.params)
+            visitVarDecl(vd);
+        visitBlock(fd.block);
 
-	@Override
-	public Void visitStructType(StructType st) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitPointerType(PointerType st) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitArrayType(ArrayType at) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitIntLiteral(IntLiteral il) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitStrLiteral(StrLiteral sl) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitChrLiteral(ChrLiteral cl) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitBinOp(BinOp bo) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitOp(Op o) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitArrayAccessExpr(ArrayAccessExpr aa) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitFieldAccessExpr(FieldAccessExpr fa) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitValueAtExpr(ValueAtExpr va) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitSizeOfExpr(SizeOfExpr so) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitTypecastExpr(TypecastExpr tc) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitExprStmt(ExprStmt es) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitWhile(While w) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitIf(If i) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitAssign(Assign a) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitReturn(Return r) {
-		// To be completed...
-		return null;
-	}
+        return null;
+    }
 
 
-	// To be completed...
+    @Override
+    public Void visitProgram(Program p) {
+        for (StructTypeDecl std : p.structTypeDecls)
+            visitStructTypeDecl(std);
+        for (VarDecl vd : p.varDecls)
+            visitVarDecl(vd);
+        for (FunDecl fd : p.funDecls)
+            visitFunDecl(fd);
+        return null;
+    }
+
+    @Override
+    public Void visitVarDecl(VarDecl vd) {
+        Symbol s = scope.lookupCurrent(vd.varName);
+        if (s != null)
+            error("Variable " + vd.varName + " already declared!");
+        else
+            scope.put(new VarSymbol(vd));
+        return null;
+    }
+
+    @Override
+    public Void visitVarExpr(VarExpr v) {
+        Symbol s = scope.lookup(v.name);
+        if (s == null) {
+			error("Variable " + v.name + " not declared!");
+		} else if (!s.isVar()) {
+        	error(v.name + "is not declared as a variable");
+		}
+        return null;
+    }
+
+    @Override
+    public Void visitFunCallExpr(FunCallExpr fc) {
+        Symbol s = scope.lookup(fc.name);
+        if (s == null) {
+			error("Function " + fc.name + " not declared!");
+		} else if (!s.isFunc()) {
+        	error(fc.name + "is not declared as a function");
+		}
+        return null;
+    }
+
+    @Override
+    public Void visitStructType(StructType st) {
+        Symbol s = scope.lookup(st.name);
+        if (s == null) {
+			error("StructType " + st.name + " not declared!");
+        } else if (!s.isStruct()) {
+			error(st.name + "is not declared as a StructType");
+		}
+        return null;
+    }
+
+    @Override
+    public Void visitPointerType(PointerType pt) {
+        return null;
+    }
+
+    @Override
+    public Void visitArrayType(ArrayType at) {
+
+        return null;
+    }
+
+    public void visitExpression(Expr expression) {
+
+    }
+
+    @Override
+    public Void visitIntLiteral(IntLiteral il) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitStrLiteral(StrLiteral sl) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitChrLiteral(ChrLiteral cl) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitBinOp(BinOp bo) {
+        visitExpression(bo.lhs);
+        visitExpression(bo.rhs);
+        return null;
+    }
+
+    @Override
+    public Void visitOp(Op o) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitArrayAccessExpr(ArrayAccessExpr aa) {
+        visitExpression(aa.name);
+        visitExpression(aa.index);
+        return null;
+    }
+
+    @Override
+    public Void visitFieldAccessExpr(FieldAccessExpr fa) {
+        visitExpression(fa.name);
+        return null;
+    }
+
+    @Override
+    public Void visitValueAtExpr(ValueAtExpr va) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitSizeOfExpr(SizeOfExpr so) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitTypecastExpr(TypecastExpr tc) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitExprStmt(ExprStmt es) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitWhile(While w) {
+        visitExpression(w.expression);
+        visitStatement(w.statement);
+        return null;
+    }
+
+    @Override
+    public Void visitIf(If i) {
+        visitExpression(i.expression);
+        visitStatement(i.ifStatement);
+        if (i.elseStatement != null)
+            visitStatement(i.elseStatement);
+        return null;
+    }
+
+    @Override
+    public Void visitAssign(Assign a) {
+        // To be completed...
+        return null;
+    }
+
+    @Override
+    public Void visitReturn(Return r) {
+        // To be completed...
+        return null;
+    }
+
+
+    // To be completed...
 
 
 }
--- src/sem/Symbol.java
+++ src/sem/Symbol.java
@@ -3,8 +3,19 @@ package sem;
 public abstract class Symbol {
 	public String name;
 	
-	
 	public Symbol(String name) {
 		this.name = name;
+	}
+
+	public boolean isVar() {
+		return this.getClass().equals(VarSymbol.class);
+	}
+
+	public boolean isFunc() {
+		return this.getClass().equals(FuncSymbol.class);
+	}
+
+	public boolean isStruct() {
+		return this.getClass().equals(StructTypeSymbol.class);
 	}
 }
