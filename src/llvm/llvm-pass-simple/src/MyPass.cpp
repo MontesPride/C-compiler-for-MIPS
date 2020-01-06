1d8035c0ee2539ae087e921e18a4c02685d8c052
Szymon Filipiak <szymonfilipiak98@gmail.com>
2020-01-06 12:58:33 +0100

    Simple dead code elimination pass. Init commit.

--- src/llvm/llvm-pass-simple/src/MyPass.cpp
+++ src/llvm/llvm-pass-simple/src/MyPass.cpp
@@ -6,17 +6,72 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Transforms/Utils/Local.h"
+#include <vector>
 
 using namespace llvm;
+using namespace std;
 
 namespace {
 struct MyPass : public FunctionPass {
   static char ID;
   MyPass() : FunctionPass(ID) {}
 
-  bool runOnFunction(Function &F) override {
-    errs() << "I saw a function called " << F.getName() << "!\n";
-    return true;
+  //-----------------------------------------------
+  /*
+      bool runOnFunction(Function &F) override {
+      errs() << "I saw a function called " << F.getName() << "!\n";
+      return true;
+      }
+      */
+  //------------------------------------------------
+
+  // findAndRemoveDeadFunctions returns true if at least one instruction was found trivially dead
+  bool findAndRemoveDeadFunctions(Function& F) {
+    SmallVector<Instruction*, 64> workList;
+
+    // Find trivially dead instructions
+    for (Function::iterator bb = F.begin(); bb != F.end(); bb++) {
+      for (BasicBlock::iterator i = bb->begin(); i != bb->end(); i++) {
+        Instruction* instruction = &*i;
+
+        if (isInstructionTriviallyDead(instruction)) {
+          errs() << "instruction dead: ";
+          instruction->printAsOperand(errs());
+          errs() << "\n";
+          workList.push_back(instruction);
+        } else {
+          errs() << "instruction alive: ";
+          instruction->printAsOperand(errs());
+          errs() << "\n";
+        }
+      }
+    }
+
+    // Remove dead instruction
+    for (Instruction* instruction : workList) {
+      errs() << "instruction removed\n";
+      instruction->eraseFromParent();
+    }
+
+    return !workList.empty();
+  }
+
+  bool runOnFunction(Function& F) override {
+    int pass = 1;
+    bool result = false;
+    bool changed = false;
+
+    do {
+      errs() << "\nI saw a function called: " << F.getName() << " (pass " << pass << ")\n";
+      pass++;
+      changed = findAndRemoveDeadFunctions(F);
+      result |= changed;
+    } while (changed);
+
+    errs() << "\n--------\n";
+
+    return result;
   }
 };
 }
